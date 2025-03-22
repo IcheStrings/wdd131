a430e60d8defda9a30250f078461a34dd2017dc6
IcheStrings <git config --global user.email>
2025-03-22 08:30:55 +0100

    updated for submission

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Set current year in the footer
     document.getElementById("year").textContent = new Date().getFullYear();
-    
+
     // Set last modified date
-    document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+    let lastModified = new Date(document.lastModified);
+    document.getElementById("lastModified").textContent = lastModified.toLocaleString();
 });
